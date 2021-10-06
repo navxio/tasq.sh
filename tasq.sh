7a61055af2a130fd96fecfda993ca360099258ec
Nav Saini <nav@widgetic.com>
2021-10-06 22:24:10 +0530

    Flags->Usage

--- tasq.sh
+++ tasq.sh
@@ -14,7 +14,7 @@ while [[ "$#" -gt 0 ]]; do
   case $1 in
     -a|--add) echo $2 >> $tasks_store; echo "added: \"$2\""; shift ;;
     -x|--mark-done) echo "done: $(head -n 1 $tasks_store)"; sed 1d -i $tasks_store; shift ;;
-    -h|--help) printf "tasq: a simple task manager\nFlags\n-a, --add <task text> to add a new task\n-x, --mark-done to mark the latest task as done and remove it from the task queue\n"; shift ;;
+    -h|--help) printf "tasq: a simple task manager\nUsage\n-a, --add <task text> to add a new task\n-x, --mark-done to mark the latest task as done and remove it from the task queue\n"; shift ;;
     *) printf "Unknown parameter passed: $1\nUse -h to print help text\n"; exit 1;;
   esac
   shift
